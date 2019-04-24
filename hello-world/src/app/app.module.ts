468ceec7f498054a6c194644946726a1b6ad9f52
siyong.zhu <siyong.zhu@irdeto.com>
2019-04-24 01:24:52 -0400

    First Angular App Component

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'courses', // this is CSS selector, and we want to select a courses element <courses>
+    template: '<h2>Courses</h2>' //whenever html has <courses>, template will be put inside of it
+})
+export class CourseComponent {
+
+}
