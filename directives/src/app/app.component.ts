f61fe6bab27fb8849baa5dab7f3bb358c3c60697
siyong.zhu <siyong.zhu@irdeto.com>
2019-05-08 02:15:33 -0400

    ngIf, ngSwitch, ngForngClass, ngStyle CustomDirective

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -7,5 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directives';
-  courses = [1, 2]
+  buttonLeftMargin = "0.5em"
+  isSelected:boolean = false
+  courses = []
+  viewMode = 'map'
+  canSave:boolean = true
+
+  task={
+    title: 'Review',
+    assignee: null
+  }
+
+  onAdd(){
+    this.courses.push({ id: 4, name: 'course4' })
+  }
+
+  onRemove(course){
+    let index = this.courses.indexOf(course)
+    this.courses.splice(index, 1)
+  }
+
+  loadCourses(){
+    this.courses = [
+      { id: 1, name: 'course1' },
+      { id: 2, name: 'course2' },
+      { id: 3, name: 'course3' },
+    ]
+  }
+
+  trackCourse(index, course){
+    return course ? course.id: undefined
+  }
+
+  onClick(){
+    this.isSelected = !this.isSelected
+  }
 }
--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
